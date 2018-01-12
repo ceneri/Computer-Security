04f28aa020062cd3592e006b2ef2cc58e0251d27
ceneri <ceneri@ucsc.edu>
2018-01-12 15:08:37 -0800

    Crack Multiple started

--- crack.c
+++ crack.c
@@ -11,7 +11,30 @@ char alphabet[] = {48,49,50,51,52,53,54,55,56,57,
                     97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,
                     117,118,119,120,121,122};
                     
-                    
+  
+
+/**
+ * Returns the number of lines in the file at path
+ * FNAME if it exists and can be read, -1 otherwise.
+ * 
+ * https://stackoverflow.com/questions/12733105
+ */
+int countLinesInFile(char *fname) {
+    FILE *fp = fopen(fname, "r");
+    int lines = -1;
+    if (fp) {
+        lines = 0;
+        while (!feof(fp)) {
+            if (fgetc(fp) == '\n') {
+                lines++;
+            }   
+        }
+        fclose(fp);
+    }
+    return lines;
+}
+
+ 
                     
 
 //https://stackoverflow.com/questions/5935933/dynamically-create-an-array-of-strings-with-malloc
@@ -42,7 +65,35 @@ char** populateTwoCharCombos(int numberOfBasicCombos) {
     }
     
     return basicCombos;
-}                   
+}  
+
+
+char** getCryptPasswds(char *fname, int numUsers) { 
+
+     
+    char **cPasswords = malloc(numUsers * sizeof (char*));
+    for (int i = 0; i < numUsers; i++){
+        cPasswords[i] = malloc( 14 * sizeof (char));
+        cPasswords[i][13] = '\0';
+    }
+
+    char line[64];
+    FILE *fin = fopen(fname, "r");
+    for (int i = 0; fgets(line, 64, fin) != NULL; i++) {
+        strtok(line, ":");
+        sprintf(cPasswords[i], "%s", strtok(strtok(NULL, ":"), "\n"));
+        printf("Password: %s \n", cPasswords[i]);
+        if ((int) strlen(cPasswords[i]) != 13) {
+            printf("Password: %s \n", cPasswords[i]);
+            printf("No: %d \n", (int) strlen(cPasswords[i]));
+            printf("ERROR\n");
+            exit(-1);
+        }
+    }
+    fclose(fin);
+    
+    return cPasswords;
+}                  
                     
 
 /*
@@ -61,57 +112,59 @@ void crackSingle(char *username, char *cryptPasswd, int pwlen, char *passwd) {
     strncpy(salt, cryptPasswd, 2);
     salt[2] = '\0';
     
-    //
-    char potentialPasswd[4+1];
+    //Account for null at end of string
+    char potentialPasswd[5];
     potentialPasswd[4] = '\0';
     
     //Populate Combos
     sem_wait(&lock);
-    char **basicCombos2;
+    char **basicCombos;
     int numberOfTwoCharCombos = sizeof(alphabet) * sizeof(alphabet);
-    basicCombos2 = populateTwoCharCombos(numberOfTwoCharCombos);
+    basicCombos = populateTwoCharCombos(numberOfTwoCharCombos);
     sem_post(&lock);
     
-    
-    //Brute Force 
-    sem_wait(&lock);
-    
-    bool onlyThreeChars = (pwlen == 3);
-
+    //Indexes depending on password size
     int threeCharIndices[] = {0,1,2,3};
     int fourCharIndices[] = {1,2,3,4};
     
+    bool onlyThreeChars = (pwlen == 3);
+        
+    //Brute Force 
+    sem_wait(&lock);
     
     for (int k = 0; k < sizeof(alphabet); k++){
         
+        //First character
         potentialPasswd[0] = alphabet[k];
+        //Default
         int *charIndices = fourCharIndices;
         
+        
         if ( onlyThreeChars ){
-            k = sizeof(alphabet);
-            (*potentialPasswd)++;
+            k = sizeof(alphabet);       //Main loop will only run once
+            (*potentialPasswd)++;       //Remove first character from string
             
-            charIndices = threeCharIndices;
+            charIndices = threeCharIndices;     //New indexes
         } 
     
         for (int j = 0; j < sizeof(alphabet); j++){
             
+            //Second character
             potentialPasswd[charIndices[0]] = alphabet[j];
         
             for (int i = 0; i < numberOfTwoCharCombos; i++){
-                potentialPasswd[charIndices[1]] = basicCombos2[i][0];
-                potentialPasswd[charIndices[2]] = basicCombos2[i][1];
+                //Third and Fourth characters
+                potentialPasswd[charIndices[1]] = basicCombos[i][0];
+                potentialPasswd[charIndices[2]] = basicCombos[i][1];
                 
                 potentialPasswd[charIndices[3]] = '\0';
                 
-                //printf("ComboSS: %s \n", potentialPasswd);
-                
+                //Check if password matches
                 bool found = strcmp(cryptPasswd, crypt(potentialPasswd, salt)) == 0;
-                
                 if ( found ){
                     strcpy(passwd, potentialPasswd);
-                    printf("Password: %s ", passwd);
-                    printf("ComboSS: %s \n", potentialPasswd);
+                    //printf("Password: %s ", passwd);
+                    //printf("ComboSS: %s \n", potentialPasswd);
                     return;
                 }
             }
@@ -125,7 +178,91 @@ void crackSingle(char *username, char *cryptPasswd, int pwlen, char *passwd) {
  * Find the plain-text passwords PASSWDS of length PWLEN for the users found
  * in the old-style /etc/passwd format file at pathe FNAME.
  */
-void crackMultiple(char *fname, int pwlen, char **passwds) { } 
+void crackMultiple(char *fname, int pwlen, char **passwds) { 
+
+    //Initialize binary semaphore
+    //http://pages.cs.wisc.edu/~remzi/Classes/537/Fall2008/Notes/threads-semaphores.txt
+    sem_t lock;
+    sem_init(&lock, 0, 1);
+    
+    //numusers
+    int numUsers = countLinesInFile(fname);
+    
+    //Get encrypted passwords
+    char **cryptPasswds;
+    cryptPasswds = getCryptPasswds(fname, numUsers);
+    
+    for (int i =0; i < numUsers; i++){
+        printf("Password: %s \n", cryptPasswds[i]);
+    }
+    
+    //Obtain salt from password/user name salt
+    /*char *salt[3];
+    strncpy(salt, cryptPasswd, 2);
+    salt[2] = '\0';
+    
+    //Account for null at end of string
+    char potentialPasswd[5];
+    potentialPasswd[4] = '\0';
+    
+    //Populate Combos
+    sem_wait(&lock);
+    char **basicCombos;
+    int numberOfTwoCharCombos = sizeof(alphabet) * sizeof(alphabet);
+    basicCombos = populateTwoCharCombos(numberOfTwoCharCombos);
+    sem_post(&lock);
+    
+    //Indexes depending on password size
+    int threeCharIndices[] = {0,1,2,3};
+    int fourCharIndices[] = {1,2,3,4};
+    
+    bool onlyThreeChars = (pwlen == 3);
+        
+    //Brute Force 
+    sem_wait(&lock);
+    
+    for (int k = 0; k < sizeof(alphabet); k++){
+        
+        //First character
+        potentialPasswd[0] = alphabet[k];
+        //Default
+        int *charIndices = fourCharIndices;
+        
+        
+        if ( onlyThreeChars ){
+            k = sizeof(alphabet);       //Main loop will only run once
+            (*potentialPasswd)++;       //Remove first character from string
+            
+            charIndices = threeCharIndices;     //New indexes
+        } 
+    
+        for (int j = 0; j < sizeof(alphabet); j++){
+            
+            //Second character
+            potentialPasswd[charIndices[0]] = alphabet[j];
+        
+            for (int i = 0; i < numberOfTwoCharCombos; i++){
+                //Third and Fourth characters
+                potentialPasswd[charIndices[1]] = basicCombos[i][0];
+                potentialPasswd[charIndices[2]] = basicCombos[i][1];
+                
+                potentialPasswd[charIndices[3]] = '\0';
+                
+                //Check if password matches
+                bool found = strcmp(cryptPasswd, crypt(potentialPasswd, salt)) == 0;
+                if ( found ){
+                    strcpy(passwd, potentialPasswd);
+                    //printf("Password: %s ", passwd);
+                    //printf("ComboSS: %s \n", potentialPasswd);
+                    return;
+                }
+            }
+        }
+    }
+
+    sem_post(&lock);*/
+
+} 
 
 /*
  * Find the plain-text passwords PASSWDS of length PWLEN for the users found
