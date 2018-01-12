0680c45146c9bfa4c4cc37e623d00a0d5875adc4
ceneri <ceneri@ucsc.edu>
2018-01-12 11:24:34 -0800

    Advances gibberish

--- crack.c
+++ crack.c
@@ -3,6 +3,45 @@
 #include <stdlib.h>
 #include <string.h>
 #include <crypt.h>
+#include <semaphore.h>
+
+char alphabet[] = {48,49,50,51,52,53,54,55,56,57,
+                    65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,
+                    97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,
+                    117,118,119,120,121,122};
+                    
+                    
+                    
+char** populateTwoCharCombos(void) { 
+    //https://stackoverflow.com/questions/5935933/dynamically-create-an-array-of-strings-with-malloc
+    ///****************malloc double digits****************
+    int ID_LEN = 2;
+    int numberOfBasicCombos = 62*62;
+    char **basicCombos;
+
+    basicCombos = malloc(numberOfBasicCombos * sizeof(char*));
+    
+    for (int i = 0; i < numberOfBasicCombos; i++){
+        basicCombos[i] = malloc((ID_LEN+1) * sizeof(char));  
+        basicCombos[i][ID_LEN] = '\0';      
+    }
+    
+    int index = 0;
+    for (int i = 0; i < 62; i++){
+        for (int j = 0; j < 62; j++){
+            
+            basicCombos[index][0] = alphabet[i];
+            basicCombos[index][1] = alphabet[j];
+            
+            printf("Combo2: %s\n", basicCombos[index]);
+            
+            index++;
+        }
+    }
+    
+    return basicCombos;
+}                   
+                    
 
 /*
  * Find the plain-text password PASSWD of length PWLEN for the user USERNAME 
@@ -19,25 +58,90 @@ void crackSingle(char *username, char *cryptPasswd, int pwlen, char *passwd) {
     printf("Salty Salt: %s\n", salt );
     printf("Squidward: %s\n", cryptPasswd);
     
-    int alphabet[62];
+    //int alphabet[62];
     //Create Array of chars
-    for (int i = 48; i < 123; i++){
-        if ( i == 58 )  i = 65;
-        if ( i == 91 )  i = 97;
-        
-        alphabet[i] = i;
-        printf("Salty Salt: %c\n", alphabet[i]);
+    for (int i = 0; i < 62; i++){
+        printf("Chars: %c\n", alphabet[i]);
     }
     
     //combos
+    int alpha_index[] = {0,0,0,0}; /* Default to 00..0 up to pwlen 0's */
+    
     char combo[pwlen+1];
     combo[pwlen] = '\0';
     
-    for (int char_pos = 0; char_pos < pwlen; char_pos++){
+    
+    /******************************************
+    a_index to combo ->
+    */
+    for (int i = 0; i < pwlen; i++){
+        combo[i] = alphabet[alpha_index[i]];
+    }
+    printf("Combo: %s\n", combo);
+    //**************************************
+    
+    //printf("Alpha_Index: %d\n", alphabet[i]);
+    
+    
+    for (int char_pos = pwlen-1; char_pos > -1; char_pos--){
+        
+        for (int i = 0; i < 62; i++){
+            
+            //get possible combo
+            alpha_index[char_pos] = i;
+             /******************************************
+    a_index to combo ->
+    */
+    /*for (int i = 0; i < pwlen; i++){
+        combo[i] = alphabet[alpha_index[i]];
+    }
+    printf("Combo: %s\n", combo);*/
+    //**************************************
+        }
         
     }
     
     
+    //https://stackoverflow.com/questions/5935933/dynamically-create-an-array-of-strings-with-malloc
+    ///****************malloc double digits****************
+    /*int ID_LEN = 2;
+    int numberOfBasicCombos = 62*62;
+    char **basicCombos;
+
+    basicCombos = malloc(numberOfBasicCombos * sizeof(char*));
+    
+    for (int i = 0; i < numberOfBasicCombos; i++){
+        basicCombos[i] = malloc((ID_LEN+1) * sizeof(char));  
+        basicCombos[i][ID_LEN] = '\0';      
+    }
+    
+    int index = 0;
+    for (int i = 0; i < 62; i++){
+        for (int j = 0; j < 62; j++){
+            
+            basicCombos[index][0] = alphabet[i];
+            basicCombos[index][1] = alphabet[j];
+            
+            printf("Combo: %s\n", basicCombos[index]);
+            
+            index++;
+        }
+    }*/
+    
+    //*******
+    
+    char **basicCombos2;
+    
+    sem_t s;
+    sem_init(&s, 0, 1);
+    
+    sem_wait(&s);
+    basicCombos2 = populateTwoCharCombos();
+    sem_post(&s);
+    
+    sem_wait(&s);
+    printf("Combo999: %s\n", basicCombos2[3400]);
+    sem_post(&s);
     
 }
 
