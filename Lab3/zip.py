40e3573859f5c7ef44632ddfc1e5f21c390c486a
Cesar Neri <ceneri@ucsc.edu>
2018-02-21 20:28:42 -0800

    Zip testing script added

---
+++ Lab3/zip.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import time
+import zipfile
+
+PASSWORD = 'fluffy'
+ZIP_NAME = 'key.zip'
+FILE_NAME = 'key'
+
+OUT_FILE = 'obfuscated.txt'
+
+
+def main():
+
+    #https://stackoverflow.com/questions/7585435/best-way-to-convert-string-to-bytes-in-python-3
+    PWD = str.encode(PASSWORD)
+
+    start = time.time()
+    print("Starting timer")
+
+    with zipfile.ZipFile(ZIP_NAME) as myzip:
+        myzip.setpassword(PWD)
+        with myzip.open(FILE_NAME, 'r') as myfile:
+            reading = myfile.read()
+            #print(reading)
+
+            #Open output files
+            file = open(OUT_FILE, "wb")
+            file.write(reading)
+            file.close()
+
+    end = time.time()
+    print(end - start, "seconds")               
+
+
+if __name__ == '__main__':
+    main()
+
+    
